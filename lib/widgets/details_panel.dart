68d97bb5a7cb214d2e8aceb5064f815e2a41b984
Ross Wang <imagipioneer@gmail.com>
2022-11-29 17:21:22 -0800

    [details] Add padding and center images and captions

--- lib/widgets/details_panel.dart
+++ lib/widgets/details_panel.dart
@@ -13,34 +13,65 @@ class DetailsPanel extends StatelessWidget {
   Widget build(BuildContext context) => station?.details == null
       ? Container()
       : SingleChildScrollView(
-          child: Html(
-            data: station!.details,
-            style: {
-              '[style*="visibility:hidden"]': Style(color: Colors.transparent)
-            },
-            customRenders: {
-              tagMatcher('img'): CustomRender.widget(
-                widget: (context, childBuilder) {
-                  // Prefer setting width rather than height to keep display
-                  // more consistent. Flutter will instead prefer height by
-                  // default. On web, this actually warps the aspect ratio for
-                  // some images, which we probably shouldn't replicate.
-                  final width = Optional(context.tree.attributes['width'])
-                      .map(double.parse);
-                  final height = width != null
-                      ? null
-                      : Optional(context.tree.attributes['height'])
-                          .map(double.parse);
-
-                  return Image(
-                    image: client
-                        .getImage(Uri.parse(context.tree.attributes['src']!)),
-                    width: width,
-                    height: height,
-                  );
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(4, 4, 8, 4),
+            child: LayoutBuilder(
+              builder: (context, boxConstraints) => Html(
+                data: station!.details,
+                style: {
+                  'body': Style(fontSize: FontSize(16)),
+                  '[style*="visibility:hidden"]':
+                      Style(color: Colors.transparent),
+                  '.details_caption': Style(
+                    // flutter_html#1146: Ideally this would be block, but that
+                    // sometimes introduces spurious newlines. Unit.percent
+                    // doesn't seem to work either.
+                    display: Display.inlineBlock,
+                    width: Width(boxConstraints.maxWidth),
+                    fontSize: FontSize(.9, Unit.em),
+                    fontStyle: FontStyle.italic,
+                    textAlign: TextAlign.center,
+                  ),
                 },
-              )
-            },
+                customRenders: {
+                  tagMatcher('img'): CustomRender.widget(
+                    widget: (context, childBuilder) {
+                      final detailsImg =
+                          context.tree.elementClasses.contains('details_img');
+
+                      // Prefer setting width rather than height to keep display
+                      // more consistent. Flutter will instead prefer height by
+                      // default. On web, this actually warps the aspect ratio
+                      // for some images, which we probably shouldn't replicate.
+                      //
+                      // Ideally we'd fill the width, but these images tend to
+                      // actually only be about 400px wide.
+                      final width = Optional(context.tree.attributes['width'])
+                          .map(double.parse);
+                      final height = width != null
+                          ? null
+                          : Optional(context.tree.attributes['height'])
+                              .map(double.parse);
+
+                      final image = Image(
+                        image: client.getImage(
+                            Uri.parse(context.tree.attributes['src']!)),
+                        width: width,
+                        height: height,
+                      );
+
+                      return detailsImg
+                          ? Align(
+                              alignment: Alignment.center,
+                              heightFactor: 1,
+                              child: image,
+                            )
+                          : image;
+                    },
+                  )
+                },
+              ),
+            ),
           ),
         );
 }
