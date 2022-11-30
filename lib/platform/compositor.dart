14951d90ccbd31ea2e695a5ce4be86cf79309a37
Ross Wang <imagipioneer@gmail.com>
2022-11-30 12:00:07 -0800

    [map] Fix web compositor
    Turns out having the `as` on the `import` only on the first part seems to make it only apply to the first part or something.

--- lib/platform/compositor.dart
+++ lib/platform/compositor.dart
@@ -1,11 +1,12 @@
 import 'dart:typed_data';
 import 'dart:ui';
 
-import 'compositor_sky.dart' as impl
-    if (dart.library.html) '../platform/compositor_web.dart';
+import 'compositor_sky.dart' if (dart.library.html) 'compositor_web.dart'
+    as impl;
 
 abstract class CompositorImage {
-  static Future<CompositorImage> Function(Uint8List data) decode = impl.CompositorImage.decode;
+  static Future<CompositorImage> Function(Uint8List data) decode =
+      impl.CompositorImage.decode;
 
   CompositorImage clone();
   void dispose();
@@ -15,7 +16,7 @@ abstract class Compositor {
   factory Compositor(int size) => impl.Compositor(size);
 
   int get size;
-  
+
   void save();
   void restore();
   void scale(double factor);
