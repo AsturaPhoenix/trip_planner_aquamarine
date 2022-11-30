09e53351beb5e72706167e43ffaa21bcd309b59a
Ross Wang <imagipioneer@gmail.com>
2022-11-30 01:32:42 -0800

    [details] Remove scrollbar padding on platforms where the scrollbar is hidden

--- lib/widgets/details_panel.dart
+++ lib/widgets/details_panel.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 
@@ -10,68 +11,77 @@ class DetailsPanel extends StatelessWidget {
   final Station? station;
 
   @override
-  Widget build(BuildContext context) => station?.details == null
-      ? Container()
-      : SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(4, 4, 8, 4),
-            child: LayoutBuilder(
-              builder: (context, boxConstraints) => Html(
-                data: station!.details,
-                style: {
-                  'body': Style(fontSize: FontSize(16)),
-                  '[style*="visibility:hidden"]':
-                      Style(color: Colors.transparent),
-                  '.details_caption': Style(
-                    // flutter_html#1146: Ideally this would be block, but that
-                    // sometimes introduces spurious newlines. Unit.percent
-                    // doesn't seem to work either.
-                    display: Display.inlineBlock,
-                    width: Width(boxConstraints.maxWidth),
-                    fontSize: FontSize(.9, Unit.em),
-                    fontStyle: FontStyle.italic,
-                    textAlign: TextAlign.center,
-                  ),
-                },
-                customRenders: {
-                  tagMatcher('img'): CustomRender.widget(
-                    widget: (context, childBuilder) {
-                      final detailsImg =
-                          context.tree.elementClasses.contains('details_img');
+  Widget build(BuildContext context) {
+    final hasScrollbar = const {
+      TargetPlatform.linux,
+      TargetPlatform.macOS,
+      TargetPlatform.windows
+    }.contains(defaultTargetPlatform);
 
-                      // Prefer setting width rather than height to keep display
-                      // more consistent. Flutter will instead prefer height by
-                      // default. On web, this actually warps the aspect ratio
-                      // for some images, which we probably shouldn't replicate.
-                      //
-                      // Ideally we'd fill the width, but these images tend to
-                      // actually only be about 400px wide.
-                      final width = Optional(context.tree.attributes['width'])
-                          .map(double.parse);
-                      final height = width != null
-                          ? null
-                          : Optional(context.tree.attributes['height'])
-                              .map(double.parse);
+    return station?.details == null
+        ? Container()
+        : SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(4, 4, hasScrollbar ? 8 : 4, 4),
+              child: LayoutBuilder(
+                builder: (context, boxConstraints) => Html(
+                  data: station!.details,
+                  style: {
+                    'body': Style(fontSize: FontSize(16)),
+                    '[style*="visibility:hidden"]':
+                        Style(color: Colors.transparent),
+                    '.details_caption': Style(
+                      // flutter_html#1146: Ideally this would be block, but that
+                      // sometimes introduces spurious newlines. Unit.percent
+                      // doesn't seem to work either.
+                      display: Display.inlineBlock,
+                      width: Width(boxConstraints.maxWidth),
+                      fontSize: FontSize(.9, Unit.em),
+                      fontStyle: FontStyle.italic,
+                      textAlign: TextAlign.center,
+                    ),
+                  },
+                  customRenders: {
+                    tagMatcher('img'): CustomRender.widget(
+                      widget: (context, childBuilder) {
+                        final detailsImg =
+                            context.tree.elementClasses.contains('details_img');
 
-                      final image = Image(
-                        image: client.getImage(
-                            Uri.parse(context.tree.attributes['src']!)),
-                        width: width,
-                        height: height,
-                      );
+                        // Prefer setting width rather than height to keep display
+                        // more consistent. Flutter will instead prefer height by
+                        // default. On web, this actually warps the aspect ratio
+                        // for some images, which we probably shouldn't replicate.
+                        //
+                        // Ideally we'd fill the width, but these images tend to
+                        // actually only be about 400px wide.
+                        final width = Optional(context.tree.attributes['width'])
+                            .map(double.parse);
+                        final height = width != null
+                            ? null
+                            : Optional(context.tree.attributes['height'])
+                                .map(double.parse);
 
-                      return detailsImg
-                          ? Align(
-                              alignment: Alignment.center,
-                              heightFactor: 1,
-                              child: image,
-                            )
-                          : image;
-                    },
-                  )
-                },
+                        final image = Image(
+                          image: client.getImage(
+                            Uri.parse(context.tree.attributes['src']!),
+                          ),
+                          width: width,
+                          height: height,
+                        );
+
+                        return detailsImg
+                            ? Align(
+                                alignment: Alignment.center,
+                                heightFactor: 1,
+                                child: image,
+                              )
+                            : image;
+                      },
+                    )
+                  },
+                ),
               ),
             ),
-          ),
-        );
+          );
+  }
 }
